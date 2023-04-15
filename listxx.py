64e01ba8551a726e1a9452d4c1dc8888c5816e61
gccodes38 <127054630+gccodes38@users.noreply.github.com>
2023-04-15 13:19:47 +0100

    Add files via upload

---
+++ listxx.py
@@ -0,0 +1,11 @@
+#list of item containing 15 items
+list = [34, 3, 23, 12, 8, 9, 10, 14, 5, 13, 50, 26, 18, 31, 16]
+
+#command to print the length of the item
+print(len(list))
+
+#command to print both the largest and the smallest number from the list
+print("this is the largest number: ", max(list))
+print("this is the smallest number: ", min(list))
+
+
