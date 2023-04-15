3b72378a822e36c73580b72dbe6ae8896559b245
gccodes38 <127054630+gccodes38@users.noreply.github.com>
2023-04-15 13:19:16 +0100

    Add files via upload

---
+++ listx.py
@@ -0,0 +1,10 @@
+#list of 9 items
+list = [7, 8, 20, 8, 5, 13, 15, 14, 13]
+
+ # removal of duplicate
+item = set(list)
+print("this is the non-duplicated values:",item)
+
+#summation of all the items
+summation = sum(item)
+print("Total = ",summation)
